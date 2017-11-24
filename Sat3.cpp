0ac56097c93d62b7fa89b71b10add860a4c83aeb
Imar Abreu Diaz <alu0100888447@ull.edu.es>
2017-11-24 10:02:37 +0000

    Creacion del proyecto

---
+++ Sat3.cpp
@@ -0,0 +1,5 @@
+//
+// Created by izm20 on 24/11/17.
+//
+
+#include "Sat3.h"
---
+++ Sat3.h
@@ -0,0 +1,14 @@
+//
+// Created by izm20 on 24/11/17.
+//
+
+#ifndef PRACTICA4_SAT3_H
+#define PRACTICA4_SAT3_H
+
+
+class Sat3 {
+
+};
+
+
+#endif //PRACTICA4_SAT3_H
